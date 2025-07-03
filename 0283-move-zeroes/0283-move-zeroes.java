0d41c0e6968333269a7166cd8a0005392d9271e9
Shreyash Dhage <sdhage1502@gmail.com>
2025-07-03 19:19:36 +0530

    Time: 3 ms (21.96%), Space: 46.4 MB (12.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,25 +1,39 @@
 class Solution {
-
-    public void moveZeroes(int[] nums) {
-        int l = 0;
-        int r = 1;
-
-        while (l <= r && r <= nums.length - 1) {
-            if (nums[l] == 0) {
-                if (nums[r] != 0) {
-                    int temp = nums[l];
-                    nums[l] = nums[r];
-                    nums[r] = temp;
-                } else {
-                    r++;
-                }
-
-            } else if (nums[l] != 0 || nums[r] != 0) {
-                l++;
-                r++;
+     public void moveZeroes(int[] nums) {
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
             }
-
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
+            }
         }
-
     }
-}
+}// class Solution {
+
+//     public void moveZeroes(int[] nums) {
+//         int l = 0;
+//         int r = 1;
+
+//         while (l <= r && r <= nums.length - 1) {
+//             if (nums[l] == 0) {
+//                 if (nums[r] != 0) {
+//                     int temp = nums[l];
+//                     nums[l] = nums[r];
+//                     nums[r] = temp;
+//                 } else {
+//                     r++;
+//                 }
+
+//             } else if (nums[l] != 0 || nums[r] != 0) {
+//                 l++;
+//                 r++;
+//             }
+
+//         }
+
+//     }
+// }
